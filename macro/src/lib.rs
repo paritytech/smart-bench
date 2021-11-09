b36d7283d4d9940a719e61077a74ec508e8bbe9e
Andrew Jones <ascjones@gmail.com>
2021-11-09 15:44:11 +0000

    Constructor and message generators

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -20,16 +20,87 @@ pub fn contract(input: TokenStream) -> TokenStream {
     let metadata: ContractMetadata = serde_json::from_reader(reader)
         .unwrap_or_else(|e| abort_call_site!("Failed to deserialize metadata file: {}", e));
 
-    let contract_mod = generate_contract_mod(metadata.v1);
+    let contract_mod = generate_contract_mod(metadata);
     contract_mod.into()
 }
 
 #[derive(Deserialize)]
 struct ContractMetadata {
+    contract: Contract,
     #[serde(rename = "V1")]
     pub v1: ink_metadata::InkProject,
 }
 
-fn generate_contract_mod(metadata: ink_metadata::InkProject) -> proc_macro2::TokenStream {
-    quote::quote!()
+/// Metadata about a smart contract.
+#[derive(Deserialize)]
+struct Contract {
+    name: String,
+}
+
+fn generate_contract_mod(metadata: ContractMetadata) -> proc_macro2::TokenStream {
+    let contract_name = quote::format_ident!("{}", metadata.contract.name);
+    let constructors = generate_constructors(&metadata.v1);
+    let messages = generate_messages(&metadata.v1);
+
+    quote::quote!(
+        pub mod #contract_name {
+            pub mod constructors {
+                #( #constructors )*
+            }
+
+            pub mod messages {
+                #( #messages )*
+            }
+        }
+    )
+}
+
+fn generate_constructors(metadata: &ink_metadata::InkProject) -> Vec<proc_macro2::TokenStream> {
+    metadata
+        .spec()
+        .constructors()
+        .iter()
+        .map(|constructor| {
+            let name = &constructor.name().last().expect("Message should have a name");
+            let fn_ident = quote::format_ident!("{}", name);
+            let args = constructor
+                .args
+                .iter()
+                .map(|arg| {
+                    let name = quote::format_ident!("{}", arg.name());
+                    let ty = quote::quote!( u8 ); // todo
+                    quote::quote!( #name: #ty )
+                });
+            quote::quote! (
+                pub fn #fn_ident(#( #args ), *) -> ::std::vec::Vec<u8> {
+                    todo!()
+                }
+            )
+        })
+        .collect()
+}
+
+fn generate_messages(metadata: &ink_metadata::InkProject) -> Vec<proc_macro2::TokenStream> {
+    metadata
+        .spec()
+        .messages()
+        .iter()
+        .map(|message| {
+            let name = &message.name().last().expect("Message should have a name");
+            let fn_ident = quote::format_ident!("{}", name);
+            let args = message
+                .args()
+                .iter()
+                .map(|arg| {
+                    let name = quote::format_ident!("{}", arg.name());
+                    let ty = quote::quote!( u8 ); // todo
+                    quote::quote!( #name: #ty )
+                });
+            quote::quote! (
+                pub fn #fn_ident(#( #args ), *) -> ::std::vec::Vec<u8> {
+                    todo!()
+                }
+            )
+        })
+        .collect()
 }
