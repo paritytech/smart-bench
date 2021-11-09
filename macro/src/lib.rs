c5dc9f08f5e61fa378b34c9009ee90bf722cae74
Andrew Jones <ascjones@gmail.com>
2021-11-09 14:45:24 +0000

    Decode contract metadata

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -5,6 +5,7 @@ use proc_macro_error::{
     proc_macro_error
 };
 use proc_macro::TokenStream;
+use serde::Deserialize;
 
 #[proc_macro]
 #[proc_macro_error]
@@ -16,23 +17,18 @@ pub fn contract(input: TokenStream) -> TokenStream {
 
     let reader = std::fs::File::open(metadata_path)
         .unwrap_or_else(|e| abort_call_site!("Failed to read metadata file: {}", e));
-    let metadata: ink_metadata::MetadataVersioned = serde_json::from_reader(reader)
+    let metadata: ContractMetadata = serde_json::from_reader(reader)
         .unwrap_or_else(|e| abort_call_site!("Failed to deserialize metadata file: {}", e));
 
-    if let ink_metadata::MetadataVersioned::V1(ink_project) = metadata {
-        let contract_mod = generate_contract_mod(ink_project);
-        contract_mod.into()
-    } else {
-        abort_call_site!("Invalid ink! metadata version: expected V1")
-    }
-
+    let contract_mod = generate_contract_mod(metadata.v1);
+    contract_mod.into()
 }
 
-// #[derive(Clone, Debug, Deserialize)]
-// pub struct ContractMetadata {
-//     #[serde(flatten)]
-//     pub abi: ink_metadata::MetadataVersioned,
-// }
+#[derive(Deserialize)]
+struct ContractMetadata {
+    #[serde(rename = "V1")]
+    pub v1: ink_metadata::InkProject,
+}
 
 fn generate_contract_mod(metadata: ink_metadata::InkProject) -> proc_macro2::TokenStream {
     quote::quote!()
