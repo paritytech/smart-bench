c35f6e8af0a92fe0daa5be512ca2ab455260b5c3
Andrew Jones <ascjones@gmail.com>
2021-11-10 16:02:13 +0000

    Mutliple calls per instance

--- src/main.rs
+++ src/main.rs
@@ -26,18 +26,18 @@ async fn main() -> color_eyre::Result<()> {
     let code =
         std::fs::read("/home/andrew/code/paritytech/ink/examples/erc20/target/ink/erc20.wasm")?;
 
-    let instance_count = 5;
+    let instance_count = 30;
     let contract_accounts = erc20_instantiate(&mut alice, code, instance_count).await?;
 
     println!("Instantiated {} erc20 contracts", contract_accounts.len());
 
     let _block_subscription = canvas::BlocksSubscription::new().await?;
 
-    let tx_hashes = erc20_transfer(&mut alice,&bob, 1, contract_accounts).await?;
+    let tx_hashes = erc20_transfer(&mut alice,&bob, 1, contract_accounts, 3).await?;
 
     println!("Submitted {} erc20 transfer calls", tx_hashes.len());
 
-    async_std::task::sleep(std::time::Duration::from_secs(10)).await;
+    async_std::task::sleep(std::time::Duration::from_secs(30)).await;
 
     Ok(())
 }
@@ -79,6 +79,7 @@ async fn erc20_transfer(
     dest: &canvas::AccountId,
     amount: canvas::Balance,
     contracts: Vec<canvas::AccountId>,
+    transfer_count: u32,
 ) -> color_eyre::Result<Vec<canvas::Hash>> {
     let gas_limit = 500_000_000_000;
 
@@ -86,9 +87,11 @@ async fn erc20_transfer(
     let mut tx_hashes = Vec::new();
 
     for contract in contracts {
-        let tx_hash = canvas::call(contract, 0, gas_limit, &transfer, &signer).await?;
-        tx_hashes.push(tx_hash);
-        signer.increment_nonce();
+        for _ in 0..transfer_count {
+            let tx_hash = canvas::call(contract, 0, gas_limit, &transfer, &signer).await?;
+            tx_hashes.push(tx_hash);
+            signer.increment_nonce();
+        }
     }
 
     Ok(tx_hashes)
