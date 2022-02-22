979904c12bd4d77fe5392947bc5c3ddc7a34d488
Andrew Jones <ascjones@gmail.com>
2022-02-22 17:11:35 +0000

    Revert "Extract erc20 methods to file"
    This reverts commit 0c84fc130dc1ac5edaed7a48781b64cff39c899d.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 mod canvas;
-mod contracts;
 
 use color_eyre::eyre;
-use contracts::erc20;
 use sp_keyring::AccountKeyring;
 use structopt::StructOpt;
 use subxt::{PairSigner, Signer as _};
@@ -26,6 +24,8 @@ pub trait InkConstructor: codec::Encode {
 pub trait InkMessage: codec::Encode {
     const SELECTOR: [u8; 4];
 }
+
+smart_bench_macro::contract!("./contracts/erc20.contract");
 
 #[async_std::main]
 async fn main() -> color_eyre::Result<()> {
@@ -57,13 +57,14 @@ async fn main() -> color_eyre::Result<()> {
 
     let api = canvas::ContractsApi::new(client);
 
-    let contract_accounts = erc20::new(&api, &mut alice, code.0, opts.instance_count).await?;
+    let contract_accounts =
+        erc20_instantiate(&api, &mut alice, code.0, opts.instance_count).await?;
 
     println!("Instantiated {} erc20 contracts", contract_accounts.len());
 
     let block_subscription = canvas::BlocksSubscription::new().await?;
 
-    let tx_hashes = erc20::transfer(
+    let tx_hashes = erc20_transfer(
         &api,
         &mut alice,
         &bob,
@@ -86,4 +87,74 @@ async fn main() -> color_eyre::Result<()> {
     }
 
     Ok(())
+}
+
+async fn erc20_instantiate(
+    api: &canvas::ContractsApi,
+    signer: &mut canvas::Signer,
+    code: Vec<u8>,
+    count: u32,
+) -> color_eyre::Result<Vec<canvas::AccountId>> {
+    let value = 0;
+    let gas_limit = 500_000_000_000;
+    let storage_deposit_limit = None;
+
+    let initial_supply = 1_000_000;
+    let constructor = erc20::constructors::new(initial_supply);
+
+    let mut accounts = Vec::new();
+    for i in 0..count {
+        let salt = i.to_le_bytes().to_vec();
+        let code = code.clone(); // subxt codegen generates constructor args by value atm
+
+        let contract = api
+            .instantiate_with_code(
+                value,
+                gas_limit,
+                storage_deposit_limit,
+                code.clone(),
+                &constructor,
+                salt,
+                signer,
+            )
+            .await?;
+        accounts.push(contract);
+        signer.increment_nonce();
+    }
+
+    Ok(accounts)
+}
+
+async fn erc20_transfer(
+    api: &canvas::ContractsApi,
+    signer: &mut canvas::Signer,
+    dest: &canvas::AccountId,
+    amount: canvas::Balance,
+    contracts: Vec<canvas::AccountId>,
+    transfer_count: u32,
+) -> color_eyre::Result<Vec<canvas::Hash>> {
+    let gas_limit = 500_000_000_000;
+    let storage_deposit_limit: Option<canvas::Balance> = None;
+
+    let transfer = erc20::messages::transfer(dest.clone(), amount);
+    let mut tx_hashes = Vec::new();
+
+    for contract in contracts {
+        for _ in 0..transfer_count {
+            let tx_hash = api
+                .call(
+                    contract.clone(),
+                    0,
+                    gas_limit,
+                    storage_deposit_limit,
+                    &transfer,
+                    signer,
+                )
+                .await?;
+            tx_hashes.push(tx_hash);
+            signer.increment_nonce();
+        }
+    }
+
+    Ok(tx_hashes)
 }
