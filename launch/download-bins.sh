9496469c20fe9119c293e427f678ca7ce03ddb0d
ascjones <ascjones@gmail.com>
2022-03-09 16:01:47 +0000

    Add script to download cumulus and polkadot binaries

---
+++ launch/download-bins.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cumulus_version=0.9.17
+polkadot_version=0.9.17-rc4
+
+mkdir bin
+curl https://github.com/paritytech/cumulus/releases/download/polkadot-v$cumulus_version/polkadot-collator --output ./bin/polkadot-collator --location
+curl https://github.com/paritytech/polkadot/releases/download/v$polkadot_version/polkadot --output ./bin/polkadot --location
+
+chmod +x ./bin/polkadot-collator
+chmod +x ./bin/polkadot
