f71b65ffa85e4994665c85bf40c1d3f05b672d5e
ascjones <ascjones@gmail.com>
2022-05-10 11:04:18 +0100

    Extract contract abi from json

--- src/moonbeam/mod.rs
+++ src/moonbeam/mod.rs
@@ -2,7 +2,7 @@ mod account;
 mod xts;
 
 use crate::Cli;
-use color_eyre::eyre;
+use color_eyre::{Section as _, eyre};
 use ethabi::Contract;
 use sp_core::{H256, U256};
 
@@ -21,12 +21,15 @@ pub async fn exec(cli: &Cli) -> color_eyre::Result<()> {
     let metadata_path = format!("{root}/contracts/solidity/{name}_meta.json");
     let code = std::fs::read(bin_path)?;
     let metadata_reader = std::fs::File::open(metadata_path)?;
+    let json: serde_json::Map<String, serde_json::Value> = serde_json::from_reader(metadata_reader)?;
+    let abi = json["output"]["abi"].clone();
+    let contract: Contract = serde_json::from_value(abi)?;
 
-    let contract = Contract::load(metadata_reader)?;
     let constructor = contract
         .constructor()
         .ok_or_else(|| eyre::eyre!("No constructor for contract found"))?;
     let data = constructor.encode_input(code.into(), &[ethabi::Token::Uint(0u32.into())])?;
+    println!("data {:?}", data);
     let salt = H256::zero();
     let value = U256::zero();
     let gas_limit = 21_000_000;
@@ -35,7 +38,8 @@ pub async fn exec(cli: &Cli) -> color_eyre::Result<()> {
 
     let contract_account = api
         .create2(data, salt, value, gas_limit, nonce, &signer)
-        .await?;
+        .await
+        .note("Error calling create2")?;
 
     println!("Created new contract {:?}", contract_account);
 
