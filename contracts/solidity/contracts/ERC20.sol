b9efee9172ea4526297ee36b2f48c3b54ad63e89
ascjones <ascjones@gmail.com>
2022-05-31 12:08:50 +0100

    Init hardhat structure

--- contracts/solidity/contracts/ERC20.sol
+++ contracts/solidity/contracts/ERC20.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-import "node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BenchERC20 is ERC20 {
     constructor() ERC20("BenchERC20", "CAN") {
---
+++ contracts/solidity/hardhat.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.0",
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+};
