96f36e28b8dae157d5bbc17b75c2f1bde218d532
ascjones <ascjones@gmail.com>
2022-08-19 16:17:24 +0100

    Remove pinning stuff

--- src/stats.rs
+++ src/stats.rs
@@ -12,33 +12,31 @@ pub struct BlockInfo {
 /// `remaining_hashes` have been received.
 pub fn collect_block_stats<'a>(
     client: &'a OnlineClient<DefaultConfig>,
-    block_stats: impl TryStream<Ok = blockstats::BlockStats, Error = Error> + Unpin + 'a,
+    block_stats: impl TryStream<Ok = blockstats::BlockStats, Error = Error> + 'a,
     mut remaining_hashes: HashSet<sp_core::H256>,
-) -> impl TryStream<Ok = BlockInfo, Error = color_eyre::Report> + Unpin + 'a {
-    Box::pin(
-        block_stats
-            .map_err(|e| color_eyre::eyre::eyre!("Block stats subscription error: {e:?}"))
-            .and_then(|stats| {
-                tracing::debug!("{stats:?}");
-                let client = client.clone();
-                async move {
-                    let block = client.rpc().block(Some(stats.hash)).await?;
-                    let extrinsics = block
-                        .unwrap_or_else(|| panic!("block {} not found", stats.hash))
-                        .block
-                        .extrinsics
-                        .iter()
-                        .map(BlakeTwo256::hash_of)
-                        .collect();
-                    Ok(BlockInfo { extrinsics, stats })
-                }
-            })
-            .try_take_while(move |block_info| {
-                for xt in &block_info.extrinsics {
-                    remaining_hashes.remove(xt);
-                }
-                let some_remaining_txs = !remaining_hashes.is_empty();
-                future::ready(Ok(some_remaining_txs))
-            }),
-    )
+) -> impl TryStream<Ok = BlockInfo, Error = color_eyre::Report> + '_ {
+    block_stats
+        .map_err(|e| color_eyre::eyre::eyre!("Block stats subscription error: {e:?}"))
+        .and_then(|stats| {
+            tracing::debug!("{stats:?}");
+            let client = client.clone();
+            async move {
+                let block = client.rpc().block(Some(stats.hash)).await?;
+                let extrinsics = block
+                    .unwrap_or_else(|| panic!("block {} not found", stats.hash))
+                    .block
+                    .extrinsics
+                    .iter()
+                    .map(BlakeTwo256::hash_of)
+                    .collect();
+                Ok(BlockInfo { extrinsics, stats })
+            }
+        })
+        .try_take_while(move |block_info| {
+            for xt in &block_info.extrinsics {
+                remaining_hashes.remove(xt);
+            }
+            let some_remaining_txs = !remaining_hashes.is_empty();
+            future::ready(Ok(some_remaining_txs))
+        })
 }
