64d368282c910afaeeb41bf66998425ec7fa96f1
Karol Kokoszka <karol.k91@gmail.com>
2024-01-21 11:04:30 +0100

    Improve TPS calculation (ignore non-call txs, ignore last block)

--- src/stats.rs
+++ src/stats.rs
@@ -6,7 +6,7 @@ use std::sync::{Arc, Mutex};
 pub struct BlockInfo {
     pub stats: blockstats::BlockStats,
     // list of hashes to look for
-    pub hashes: Vec<sp_core::H256>,
+    pub contract_call_hashes: Vec<sp_core::H256>,
 }
 
 /// Subscribes to block stats. Completes once *all* hashes in `remaining_hashes` have been received.
@@ -44,33 +44,65 @@ where
             for xt in &hashes {
                 remaining_hashes.remove(xt);
             }
-            Ok(BlockInfo { hashes, stats })
+            Ok(BlockInfo {
+                contract_call_hashes: hashes,
+                stats,
+            })
         }
     })
 }
 
-///  Print the block info stats to the console
+/// This function prints statistics to the standard output.
+
+/// The TPS calculation is based on the following assumptions about smart-bench:
+/// - smart-bench instantiates smart contracts on the chain and waits for the completion of these transactions.
+/// - Starting from some future block (after creation), smart-bench uploads transactions related to contract calls to the node.
+/// - Sending contract call transactions to the node is continuous once started and is not mixed with any other type of transactions.
+/// - Smart-bench finishes benchmarking at the block that contains the last contract call from the set.
+
+/// TPS calculation is exclusively concerned with contract calls, disregarding any system or contract-creating transactions.
+
+/// TPS calculation excludes the last block of the benchmark, as its full utilization is not guaranteed. In other words, only blocks in the middle will consist entirely of contract calls.
 pub async fn print_block_info(
     block_info: impl TryStream<Ok = BlockInfo, Error = color_eyre::Report>,
 ) -> color_eyre::Result<()> {
-    let mut total_extrinsics = 0u64;
-    let mut total_blocks = 0u64;
+    let mut call_extrinsics_per_block: Vec<u64> = Vec::new();
+    let mut call_block_expected = false;
     println!();
     block_info
         .try_for_each(|block| {
             println!("{}", block.stats);
-            total_extrinsics += block.stats.num_extrinsics;
-            total_blocks += 1;
+            let contract_calls_count = block.contract_call_hashes.len() as u64;
+            // Skip blocks at the beggining until we see first call related transaction
+            // Once first call is seen, we expect all further blocks to contain calls until all calls are covered
+            if !call_block_expected && contract_calls_count > 0 {
+                call_block_expected = true;
+            }
+            if call_block_expected {
+                call_extrinsics_per_block.push(contract_calls_count);
+            }
             future::ready(Ok(()))
         })
         .await?;
+
+    // Skip last block as its not stressed to its full cabailities since there is very low chance of hitting
+    // that exact amount of transactions (it will contain as much transactions as there are left to execute)
+    let call_extrinsics_per_block =
+        &call_extrinsics_per_block[0..call_extrinsics_per_block.len() - 1];
+
+    let tps_blocks = call_extrinsics_per_block.len();
+    let tps_total_extrinsics = call_extrinsics_per_block.iter().sum::<u64>();
     println!("\nSummary:");
-    println!("Total Blocks: {total_blocks}");
-    println!("Total Extrinsics: {total_extrinsics}");
-    println!("TPS - Transaction execution time per second, assuming a 0.5-second execution time per block");
-    println!(
-        "TPS: {}",
-        total_extrinsics as f64 / (total_blocks as f64 * 0.5)
-    );
+    println!("Total Blocks: {tps_blocks}");
+    println!("Total Extrinsics: {tps_total_extrinsics}");
+    if tps_blocks > 0 {
+        println!("TPS - Transaction execution time per second, assuming a 0.5-second execution time per block");
+        println!(
+            "TPS: {}",
+            tps_total_extrinsics as f64 / (tps_blocks as f64 * 0.5)
+        );
+    } else {
+        println!("TPS - Error - not enough data to calculate TPS, consider increasing --call-count value")
+    }
     Ok(())
 }
